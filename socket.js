28f7da47a370b1c477325efc056f5187d256f0a5
Erik Pukinskis <erik@snowedin.net>
2016-11-10 22:36:52 -0800

    add socket server

--- socket.js
+++ socket.js
@@ -5,10 +5,94 @@ var library = require("nrtv-library")(require)
 
 module.exports = library.export(
   "nrtv-socket",
-  ["browser-bridge"],
-  function(bridge) {
+  ["browser-bridge", "ws", "nrtv-server", "http"],
+  function(bridge, ws, nrtvServer, http) {
 
-    function getSocket(collective, callback, queryString) {
+
+    function SocketServer(server) {
+      if (!server) {
+        server = nrtvServer
+      }
+
+      var socketServer = server.__nrtvSocketServer
+
+      if (socketServer) {
+        throw new Error("The server already has a socket server associated with it. Do you want to do SocketServer.onServer(yourServer) instead? You can call that as many times as you want.")
+      }
+
+      this.adopters = []
+      this._takeOver(server)
+    }
+
+    SocketServer.prototype.use =
+      function(handler) {
+        this.adopters.push(handler)
+      }
+
+    SocketServer.prototype._takeOver =
+      function(server) {
+        server.__nrtvSocketServer = this
+
+        var app = server.express()
+
+        var httpServer = http.createServer(app);
+
+        var wsServer = new ws.Server({server: httpServer})
+
+        this.adopters.push(function(conn) {
+          throw new Error("unadopted conn!")
+        })
+
+        server.relenquishControl(
+          function start(port) {
+            httpServer.listen(port)
+            return httpServer
+          })
+
+        wsServer.on("connection", this._handleNewConnection.bind(this))
+      }
+
+    SocketServer.prototype._handleNewConnection =
+      function(connection) {
+        var adopters = this.adopters
+
+        var location = console.log("URLLLLLLLLL", connection.upgradeReq.url)
+
+        var i = adopters.length - 1
+
+        tryAnother()
+
+        function tryAnother() {
+          var adopter = adopters[i--]
+
+          if (adopter) {
+            adopter(connection, tryAnother)
+          }
+        }
+      }
+
+    function handleConnections(server, handler) {
+      var socketServer = server.__nrtvSocketServer
+
+      if (!socketServer) {
+        socketServer = server.__nrtvSocketServer = new SocketServer(server)
+      }
+
+      socketServer.use(handler)
+    }
+
+    function defineOnBridge(bridge) {
+      var binding = bridge.__getSocketBinding
+      if (binding) {
+        return binding
+      }
+
+      binding = bridge.__getSocketBinding = bridge.defineFunction([bridge.collective({})],getSocketInBrowser)
+
+      return binding
+    }
+
+    function getSocketInBrowser(collective, callback, queryString) {
 
       var match = document.cookie.match(/nrtvMinionId=([a-z0-9]*)/)
 
@@ -55,19 +139,8 @@ module.exports = library.export(
     }
 
     return {
-      defineGetOn: function (bridge) {
-        var binding = bridge.__getSocketBinding
-        if (binding) {
-          return binding
-        }
-
-        binding = bridge.__getSocketBinding = bridge.defineFunction(
-          [bridge.collective({})],
-          getSocket
-        )
-
-        return binding
-      }
+      defineOn: defineOnBridge,
+      handleConnections: handleConnections
     }
 
   }
--- test.js
+++ test.js
@@ -1,21 +1,78 @@
 var test = require("nrtv-test")(require)
 
+
+
+
+var test = require("nrtv-test")(require)
+var library = test.library
+
 test.using(
-  "provides a websocket interface in the browser",
+  "server receives data",
+  ["./", "ws", "nrtv-server", "querystring"],
+  function(expect, done, getSocket, WebSocket, Server, querystring) {
 
-  ["./socket", "nrtv-socket-server", "web-element", "nrtv-server", "browser-bridge", "nrtv-browse",],
-  function(expect, done, socket, socketServer, element, server, BrowserBridge, browse) {
+    var server = new Server()
+
+    var socketServer =
+      getSocket.handleConnections(
+        server,
+        function(connection, next) {
+          var params = querystring.parse(connection.upgradeReq.url.split("?")[1])
+
+          var wantIt = params.__nrtvSingleUseSocketIdentifier == "102dk102ke2"
+
+          if (wantIt) {
+            connection.on("message", expectSingle)
+          } else {
+            next()
+          }
+        }
+      )
+
+    server.start(8000)
+
+    function sendMessage(message) {
+      var ws = new WebSocket('ws://localhost:8000/echo/websocket?__nrtvSingleUseSocketIdentifier=102dk102ke2')
+
+      ws.on("open", function() {
+        ws.send(message)
+      })
+    }
+
+    sendMessage("barf")
+
+    function expectSingle(data) {
+      expect(data).to.equal("barf")
+      done()
+      server.stop()
+    }
+
+  }
+)
 
 
-    socketServer.use(
+
+
+
+test.using(
+  "sending in the browser",
+
+  ["./", "web-element", "nrtv-server", "browser-bridge", "nrtv-browse",],
+  function(expect, done, getSocket, element, Server, BrowserBridge, browse) {
+
+    var server = new Server()
+
+    getSocket.handleConnections(
+      server,
       function(connection) {
-        connection.on("data", haveExpectations)
+        connection.on("message", haveExpectations)
       }
     )
 
     var bridge = new BrowserBridge()
+
     var send = bridge.defineFunction(
-      [socket.defineGetOn(bridge)],
+      [getSocket.defineOn(bridge)],
       function pontificate(getSocket) {
         getSocket(function(socket) {
           socket.send("you'll never be yourself again")
@@ -33,6 +90,7 @@ test.using(
     var gotMessage = false
 
     browse("http://localhost:1104", function(browser) {
+
         cleanUp = function() {
           browser.done()
           server.stop()
